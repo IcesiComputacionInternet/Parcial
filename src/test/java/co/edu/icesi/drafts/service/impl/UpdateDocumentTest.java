af9f9edfd593b7a9f47b5d4566dc6c960f378726
AngelicaCorrales <72984945+AngelicaCorrales@users.noreply.github.com>
2023-03-30 15:58:06 -0500

    Update UpdateDocumentTest.java

--- src/test/java/co/edu/icesi/drafts/service/impl/UpdateDocumentTest.java
+++ src/test/java/co/edu/icesi/drafts/service/impl/UpdateDocumentTest.java
@@ -1,16 +1,22 @@
 package co.edu.icesi.drafts.service.impl;
 
+import co.edu.icesi.drafts.dto.IcesiDocumentDTO;
 import co.edu.icesi.drafts.mapper.IcesiDocumentMapper;
 import co.edu.icesi.drafts.mapper.IcesiDocumentMapperImpl;
+import co.edu.icesi.drafts.model.IcesiDocument;
+import co.edu.icesi.drafts.model.IcesiUser;
 import co.edu.icesi.drafts.repository.IcesiDocumentRepository;
 import co.edu.icesi.drafts.repository.IcesiUserRepository;
 import co.edu.icesi.drafts.service.IcesiDocumentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.*;
 
 public class UpdateDocumentTest {
 
@@ -35,5 +41,57 @@ public class UpdateDocumentTest {
     public void TestUpdate_WhenDocumentIsOnApprovedCantBeModified(){
         //TODO implement test!
         fail();
+    }
+
+    @Test
+    public void TestUpdate(){
+
+        when(documentRepository.findById(any())).thenReturn(Optional.of(defaultDocument()));
+
+        IcesiDocumentDTO icesiDocumentDTO= documentService.updateDocument(defaultDocument().getIcesiDocumentId().toString(),updateDocumentDTO());
+
+        verify(documentRepository,times(1)).findById(any());
+        verify(documentRepository,times(1)).save(any());
+        verify(documentMapper,times(1)).fromIcesiDocument(any());
+        assertEquals(updateDocumentDTO(),icesiDocumentDTO);
+
+    }
+
+    private IcesiDocumentDTO updateDocumentDTO() {
+        return IcesiDocumentDTO.builder()
+                .icesiDocumentId(UUID.fromString("2dc074a1-2100-4d49-9823-aa12de103e70"))
+                .title("Some title2")
+                .text("loreipsum loreipsum")
+                .userId(UUID.fromString("08a4db02-6625-40ee-b782-088add3a494f"))
+                .build();
+    }
+
+    private IcesiDocumentDTO defaultDocumentDTO() {
+        return IcesiDocumentDTO.builder()
+                .icesiDocumentId(UUID.fromString("2dc074a1-2100-4d49-9823-aa12de103e70"))
+                .title("Some title")
+                .text("loreipsum")
+                .userId(UUID.fromString("08a4db02-6625-40ee-b782-088add3a494f"))
+                .build();
+    }
+
+    private IcesiDocument defaultDocument() {
+        return IcesiDocument.builder()
+                .icesiDocumentId(UUID.fromString("2dc074a1-2100-4d49-9823-aa12de103e70"))
+                .title("Some title")
+                .text("loreipsum")
+                .icesiUser(defaultUser())
+                .build();
+    }
+
+    private IcesiUser defaultUser() {
+        return IcesiUser.builder()
+                .icesiUserId(UUID.fromString("08a4db02-6625-40ee-b782-088add3a494f"))
+                .email("johndoe@email.com")
+                .code("A00232323")
+                .firstName("John")
+                .lastName("Doe")
+                .phoneNumber("+57 00000000")
+                .build();
     }
 }
