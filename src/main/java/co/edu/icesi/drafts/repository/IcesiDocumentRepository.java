94acab0f84de9f0863d1f5f3e31eda6249e2949a
AngelicaCorrales <72984945+AngelicaCorrales@users.noreply.github.com>
2023-03-30 15:59:36 -0500

    Update IcesiDocumentRepository.java

--- src/main/java/co/edu/icesi/drafts/repository/IcesiDocumentRepository.java
+++ src/main/java/co/edu/icesi/drafts/repository/IcesiDocumentRepository.java
@@ -2,6 +2,7 @@ package co.edu.icesi.drafts.repository;
 
 import co.edu.icesi.drafts.model.IcesiDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.Optional;
@@ -10,6 +11,10 @@ import java.util.UUID;
 @NoRepositoryBean
 public interface IcesiDocumentRepository extends JpaRepository<IcesiDocument, UUID> {
 
+
+    @Query("SELECT document FROM IcesiDocument document WHERE  document.title= :title")
     Optional<IcesiDocument> findByTitle(String title);
 
+    @Query("SELECT document FROM IcesiDocument document WHERE  document.icesiDocumentId= :icesiDocumentId")
+    Optional<IcesiDocument> findById(UUID icesiDocumentId);
 }
