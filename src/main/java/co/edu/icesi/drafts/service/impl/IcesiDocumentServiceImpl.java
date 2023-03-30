1eadec54926ccccb6c7a843f3de5547a9a4a9cc9
juseprada96 <juseprada96@gmail.com>
2023-03-30 00:26:28 -0500

    Commit inicial

---
+++ src/main/java/co/edu/icesi/drafts/service/impl/IcesiDocumentServiceImpl.java
@@ -0,0 +1,65 @@
+package co.edu.icesi.drafts.service.impl;
+
+import co.edu.icesi.drafts.controller.IcesiDocumentController;
+import co.edu.icesi.drafts.dto.IcesiDocumentDTO;
+import co.edu.icesi.drafts.error.exception.*;
+import co.edu.icesi.drafts.error.util.IcesiExceptionBuilder;
+import co.edu.icesi.drafts.mapper.IcesiDocumentMapper;
+import co.edu.icesi.drafts.model.IcesiDocument;
+import co.edu.icesi.drafts.repository.IcesiDocumentRepository;
+import co.edu.icesi.drafts.repository.IcesiUserRepository;
+import co.edu.icesi.drafts.service.IcesiDocumentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static co.edu.icesi.drafts.error.util.IcesiExceptionBuilder.createIcesiException;
+
+
+@AllArgsConstructor
+class IcesiDocumentServiceImpl implements IcesiDocumentService {
+
+
+    private final IcesiUserRepository userRepository;
+    private final IcesiDocumentRepository documentRepository;
+    private final IcesiDocumentMapper documentMapper;
+
+    private final IcesiDocumentController documentController;
+
+    @Override
+    public List<IcesiDocumentDTO> getAllDocuments() {
+        return documentRepository.findAll().stream()
+                .map(documentMapper::fromIcesiDocument)
+                .toList();
+    }
+
+
+    @Override
+    public List<IcesiDocumentDTO> createDocuments(List<IcesiDocumentDTO> documentsDTO) {
+        return null;
+    }
+
+    @Override
+    public IcesiDocumentDTO updateDocument(String documentId, IcesiDocumentDTO icesiDocumentDTO) {
+        return null;
+    }
+
+    @Override
+    public IcesiDocumentDTO createDocument(IcesiDocumentDTO icesiDocumentDTO) {
+        var user = userRepository.findById(icesiDocumentDTO.getUserId())
+                .orElseThrow(
+                        createIcesiException(
+                                "User not found",
+                                HttpStatus.NOT_FOUND,
+                                new DetailBuilder(ErrorCode.ERR_404, "User", "Id", icesiDocumentDTO.getUserId())
+                        )
+                );
+        var icesiDocument = documentMapper.fromIcesiDocumentDTO(icesiDocumentDTO);
+        icesiDocument.setIcesiUser(user);
+        return documentMapper.fromIcesiDocument(documentRepository.save(icesiDocument));
+    }
+}
