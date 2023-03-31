da72b96a18ff7e0ba4f992fe2fe90e3e86087e9f
G-Delgado <1006170256@u.icesi.edu.co>
2023-03-30 23:49:45 -0500

    Fixed NoRepositoryBeanTag and added ComponentScan

--- src/main/java/co/edu/icesi/drafts/DraftsApplication.java
+++ src/main/java/co/edu/icesi/drafts/DraftsApplication.java
@@ -2,8 +2,10 @@ package co.edu.icesi.drafts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"co.edu.icesi.drafts.service.IcesiDocumentService"})
 public class DraftsApplication {
 
 	public static void main(String[] args) {
