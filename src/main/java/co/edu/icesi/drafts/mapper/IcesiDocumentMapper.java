7c0fa65c12ea86197d3d45e77c0c44eeedc204f1
JunaiCode <juanpalta24@hotmail.com>
2023-03-30 23:46:03 -0500

    last commit

--- src/main/java/co/edu/icesi/drafts/mapper/IcesiDocumentMapper.java
+++ src/main/java/co/edu/icesi/drafts/mapper/IcesiDocumentMapper.java
@@ -3,14 +3,11 @@ package co.edu.icesi.drafts.mapper;
 import co.edu.icesi.drafts.dto.IcesiDocumentDTO;
 import co.edu.icesi.drafts.model.IcesiDocument;
 import org.mapstruct.Mapper;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 
 
 
 @Mapper(componentModel = "spring")
 public interface IcesiDocumentMapper {
-
     IcesiDocumentDTO fromIcesiDocument(IcesiDocument icesiDocument);
 
     IcesiDocument fromIcesiDocumentDTO(IcesiDocumentDTO icesiDocumentDTO);
