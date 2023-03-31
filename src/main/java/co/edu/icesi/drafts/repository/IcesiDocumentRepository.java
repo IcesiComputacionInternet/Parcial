ae9c970b4235b67fed2efadfe3374dba9168e494
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-03-30 22:59:53 -0500

    fix-annotation @Repository of IcesiDocumentRepository fixed

--- src/main/java/co/edu/icesi/drafts/repository/IcesiDocumentRepository.java
+++ src/main/java/co/edu/icesi/drafts/repository/IcesiDocumentRepository.java
@@ -2,12 +2,12 @@ package co.edu.icesi.drafts.repository;
 
 import co.edu.icesi.drafts.model.IcesiDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@NoRepositoryBean
+@Repository
 public interface IcesiDocumentRepository extends JpaRepository<IcesiDocument, UUID> {
 
     Optional<IcesiDocument> findByTitle(String title);
